d0e3ca8be307c5c013767848ea1c2ae6345355f6
thorshan <myothunaing18@gmail.com>
2026-01-23 15:54:32 +0630

    FIX_API_CHAPTER

--- src/api/chapterApi.js
+++ src/api/chapterApi.js
@@ -1,7 +1,7 @@
 import apiClient from "./apiClient";
 
 export const chapterApi = {
-  getAllChapter: () => apiClient.get("/chapters"),
+  getAllChapter: () => apiClient.get("/chapters/admin"),
   createChapter: (data) => apiClient.post(`/chapters`, data),
   getChapter: (id) => apiClient.get(`/chapters/${id}`),
   updateChapter: (id,data) => apiClient.patch(`/chapters/${id}`, data),
